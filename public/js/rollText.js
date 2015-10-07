62d18e166f200fac14516d12fd774c1405e787c1
Kevin Cameron <kevinacameron@hotmail.com>
2015-10-07 10:03:42 -0700

    Some header styles. Fleshing out content.

---
+++ public/js/rollText.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var logo     = document.getElementsByClassName("logo")[0],
+    rollText = logo.getAttribute("data-rollText");
+
+logo.addEventListener("mouseover", function(e) {
+    
+})
