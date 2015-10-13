0e785fcbb80258c466185625b9bd0f7fcac4962a
Kevin Cameron <kevinacameron@hotmail.com>
2015-10-13 00:49:56 -0700

    Sticky footer with updated icons and a wacky :rainbow: hover effect. Toying with more fonts. Styles for big blocky links.

--- app/data/footer.js
+++ app/data/footer.js
@@ -5,26 +5,21 @@ module.exports = {
     links : [
         {
             title : "github",
-            url   : "http://github.com/kevinkace",
-            icon  : "github-square"
+            url   : "http://github.com/kevinkace"
         }, {
             title : "linkedin",
             url   : "https://www.linkedin.com/in/kevinkace",
             icon  : "linkedin-square"
         }, {
+            title : "soundcloud",
+            url   : "http://soundcloud.com",
+            icon  : "soundcloud"
+        }, {
             title : "twitter",
-            url   : "http://twitter.com/kevinkace",
-            icon  : "twitter-square"
+            url   : "http://twitter.com/kevinkace"
         }, {
             title : "instagram",
             url   : "http://instagram.com/kacekevin"
-        }, {
-            title : "soundcloud",
-            url   : "http://soundcloud.com",
-            icon  : [
-                "square",
-                "soundcloud"
-            ]
         }
     ]
 };
