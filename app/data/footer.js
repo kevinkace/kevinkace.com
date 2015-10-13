519cb9046dbc0319cf1639790a5b68c8a5171b68
Kevin Cameron <kevinacameron@hotmail.com>
2015-10-12 18:07:11 -0700

    Add icons to footer.js and update template to suit.

--- app/data/footer.js
+++ app/data/footer.js
@@ -5,19 +5,26 @@ module.exports = {
     links : [
         {
             title : "github",
-            url   : "http://github.com/kevinkace"
+            url   : "http://github.com/kevinkace",
+            icon  : "github-square"
         }, {
             title : "linkedin",
-            url   : "https://www.linkedin.com/in/kevinkace"
+            url   : "https://www.linkedin.com/in/kevinkace",
+            icon  : "linkedin-square"
         }, {
             title : "twitter",
-            url   : "http://twitter.com/kevinkace"
+            url   : "http://twitter.com/kevinkace",
+            icon  : "twitter-square"
         }, {
             title : "instagram",
             url   : "http://instagram.com/kacekevin"
         }, {
             title : "soundcloud",
-            url   : "http://soundcloud.com"
+            url   : "http://soundcloud.com",
+            icon  : [
+                "square",
+                "soundcloud"
+            ]
         }
     ]
 };
