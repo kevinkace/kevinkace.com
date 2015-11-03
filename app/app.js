416bb7124b76fbd6aca7d39ef6176341467ddd61
Kevin Cameron <kevinacameron@hotmail.com>
2015-11-02 21:43:22 -0800

    Prepended media query classes with .bp-. Added Async for stuff. Getting MD working.

--- app/app.js
+++ app/app.js
@@ -1,26 +1,58 @@
 // jshint node: true
 "use strict";
 
-var express = require("express"),
-    app     = module.exports = express(),
+var async      = require("async"),
+    express    = require("express"),
+    app        = module.exports = express(),
+    Remarkable = require("remarkable"),
+    md         = new Remarkable({
+    }),
     data    = {
         code   : require("./data/code"),
         footer : require("./data/footer")
     };
 
 
-app.set("view engine", "jade");
-app.set("views", "./app/views");
-if(app.get("env") === "development") {
-    app.locals.pretty = true;
-}
 
-app.get("/", function(req, res) {
-        res.render("pages/home", data);
-    })
-    .use(express.static("./public"));
+async.waterfall([
+    function setData(cb) {
+        cb(null, data);
+    },
+    function getLongDesc(data, cb) {
+        async.parallel([]);
+        data.code.projects.forEach(function(proj) {
+            if(!proj.descKey) {
+                proj.longDesc = proj.desc || proj.descKey;
+                return cb(null, data);
+            }
 
-app.get("/code", function(req, res) {
-        res.render("pages/code", data);
-    })
-    .use(express.static("./public"));
+            proj.longDesc = md.render(require("./data/" + proj.descKey));
+        });
+    },
+    function runApp(data, cb) {
+        app.set("view engine", "jade");
+        app.set("views", "./app/views");
+
+        if(app.get("env") === "development") {
+            app.locals.pretty = true;
+        }
+
+        app
+            .get("/", function(req, res) {
+                res.render("pages/home", data);
+            })
+            .use(express.static("./public"));
+
+        app
+            .get("/code", function(req, res) {
+                res.render("pages/code", data);
+            })
+            .use(express.static("./public"));
+    }
+], function(err, result) {
+    if(err) {
+        console.log("Crap");
+    }
+});
+
+
--- app/data/code.js
+++ app/data/code.js
@@ -3,11 +3,11 @@
 module.exports = {
     projects : [
         {
-            title  : "kevinkace.com",
-            url    : "http://github.com/kevinkace/kevinkace.com",
-            status : "production",
-            desc   : "kevinkace.md",
-            tech   : [
+            title   : "kevinkace.com",
+            url     : "http://github.com/kevinkace/kevinkace.com",
+            status  : "production",
+            descKey : "kevinkace.md",
+            tech    : [
                 "node",
                 "express",
                 "gulp",
@@ -16,50 +16,50 @@ module.exports = {
                 "responsive"
             ]
         }, {
-            title  : "Guild Wars 2",
-            url    : "http://guildwars2.com",
-            status : "production",
-            desc   : "gw2.md",
-            tech   : [
+            title   : "Guild Wars 2",
+            url     : "http://guildwars2.com",
+            status  : "production",
+            descKey : "gw2.md",
+            tech    : [
                 "wordpress",
                 "javascript",
                 "yui",
                 "jssrv"
             ]
         }, {
-            title  : "Guild Wars 2 welcome sites",
-            url    : "https://welcome.guildwars2.com/welcome",
-            status : "production",
-            desc   : "welcome.md"
-            tech   : [
+            title   : "Guild Wars 2 welcome sites",
+            url     : "https://welcome.guildwars2.com/welcome",
+            status  : "production",
+            descKey : "welcome.md",
+            tech    : [
                 "yui3",
                 "responsive",
                 "less"
             ]
         }, {
-            title  : "Dragins",
-            url    : "http://demo.kevinkace.com/dragins",
-            status : "mvp",
-            desc   : "dragins.md",
-            tech   : [
+            title   : "Dragins",
+            url     : "http://demo.kevinkace.com/dragins",
+            status  : "mvp",
+            descKey : "dragins.md",
+            tech    : [
                 "javascript",
                 "mithril"
             ]
         }, {
-            title  : "Skate Trick Tracker",
-            url    : "https://github.com/kevinkace/ttSails",
-            status : "incomplete",
-            desc   : "ttsails.md",
-            tech   : [
+            title   : "Skate Trick Tracker",
+            url     : "https://github.com/kevinkace/ttSails",
+            status  : "incomplete",
+            descKey : "ttsails.md",
+            tech    : [
                 "sails",
                 "mithril"
             ]
         }, {
-            title  : "Anime-Planet",
-            url    : "http://anime-planet.com",
-            status : "production",
-            desc   : "ap.md",
-            tech   : [
+            title   : "Anime-Planet",
+            url     : "http://anime-planet.com",
+            status  : "production",
+            descKey : "ap.md",
+            tech    : [
                 "less",
                 "dullard",
                 "jquery"
