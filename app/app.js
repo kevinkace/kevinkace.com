40bd6ad876a2abee16e2e4971e9db72c448a2526
Kevin Cameron <kevinacameron@hotmail.com>
2015-11-03 09:18:42 -0800

    MD readFile working, get control flow back in place instead of CBs.

--- app/app.js
+++ app/app.js
@@ -1,35 +1,72 @@
 // jshint node: true
 "use strict";
 
-var async      = require("async"),
+var fs         = require("fs"),
+    async      = require("async"),
     express    = require("express"),
     app        = module.exports = express(),
     Remarkable = require("remarkable"),
     md         = new Remarkable({
     }),
-    data    = {
+    data       = {
         code   : require("./data/code"),
         footer : require("./data/footer")
     };
 
 
 
-async.waterfall([
-    function setData(cb) {
-        cb(null, data);
-    },
-    function getLongDesc(data, cb) {
-        async.parallel([]);
-        data.code.projects.forEach(function(proj) {
-            if(!proj.descKey) {
-                proj.longDesc = proj.desc || proj.descKey;
-                return cb(null, data);
+// async.waterfall([
+//     function setData(cb) {
+//         cb(null, data);
+//     },
+//     function getLongDesc(data, cb) {
+//         async.each(data.code.projects, function(proj, cb) {
+//             fs.readFile("./data/" + proj.descKey, "utf-8", function(data, err) {
+//                 if(err) {
+//                     console.log("err reading " + proj.descKey);
+//                     proj.longDesc = proj.descKey;
+//                     cb();
+//                 }
+
+//                 proj.longDesc = data;
+//                 console.log("loaded " + proj.descKey);
+//                 cb();
+//             });
+//         }, function(err) {
+//             if(err) {
+//                 // this will never run
+//                 console.log(err + " not loaded");
+//             }
+//             cb();
+//         });
+//     },
+//     function runApp(data, cb) {
+
+
+    async.each(data.code.projects, function(proj, cb) {
+        var path = "./app/data/" + proj.descKey;
+
+        fs.readFile(path, "utf-8", function(err, data) {
+            if(err) {
+                console.log("err reading " + path);
+                proj.longDesc = path;
+                return cb(path);
             }
 
-            proj.longDesc = md.render(require("./data/" + proj.descKey));
+            proj.longDesc = data;
+            console.log("loaded " + path);
+            cb();
         });
-    },
-    function runApp(data, cb) {
+    }, function(err) {
+        if(err) {
+            // this will never run
+            console.log(err + " not loaded");
+        }
+        runApp();
+    });
+
+    function runApp() {
+        console.log("app aboot to run.");
         app.set("view engine", "jade");
         app.set("views", "./app/views");
 
@@ -38,21 +75,19 @@ async.waterfall([
         }
 
         app
-            .get("/", function(req, res) {
-                res.render("pages/home", data);
-            })
+            .get("/", [
+                function(req, res) {
+                    res.render("pages/home", data);
+                }
+            ])
             .use(express.static("./public"));
+    }
 
-        app
-            .get("/code", function(req, res) {
-                res.render("pages/code", data);
-            })
-            .use(express.static("./public"));
-    }
-], function(err, result) {
-    if(err) {
-        console.log("Crap");
-    }
-});
+//     }
+// ], function(err, result) {
+//     if(err) {
+//         console.log(err);
+//     }
+// });
 
 
