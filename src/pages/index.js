6186e288a576cbce0ebdf2b7d127b5ed2c90e74e
Kevin Cameron <kevinacameron@hotmail.com>
2019-03-02 22:41:50 -0800

    Update text

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,7 @@ module.exports = {
     view() {
         return m(layout,
             m("h2", "More coming soon."),
-            m("p", "Or not soon, I'm not sure."),
-            m("p", "And that email doesn't work.")
+            m("p", "In the meantime check the links below")
         );
     }
 };
