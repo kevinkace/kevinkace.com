ba70d01d05e334c549c9c527b5ba90c4e1c81e0c
kevinkace <kevinacameron@hotmail.com>
2019-03-11 11:38:38 -0700

    Add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    "gatsby-plugin-postcss"
+    "gatsby-plugin-postcss",
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-68300806-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
