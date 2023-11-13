d06a5fc2c4d67583ce4c6271888da5838a3eea0b
Kevin Cameron <kevinacameron@hotmail.com>
2023-11-12 19:10:25 -0800

    mdsvex markdown loading

--- svelte.config.js
+++ svelte.config.js
@@ -3,13 +3,22 @@ import adapter from '@sveltejs/adapter-netlify';
 // import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
+import { mdsvex } from 'mdsvex';
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions : [ '.md' ],
+};
+
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	extensions : [ '.svelte', '.md' ],
     preprocess : [
         // preprocess({
         //     // postcss : true,
         // }),
-        vitePreprocess()
+        vitePreprocess(), mdsvex(mdsvexOptions)
     ],
     kit : {
         adapter : adapter()
